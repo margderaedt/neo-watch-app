e17214d9f345526acf67a8e321ca37fac5e2530e
Margeaux DeRaedt <margderaedt@gmail.com>
2022-08-05 16:16:09 -0500

    create home screen

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { theme } from './styles/stylesheet.js';
+import WelcomeScreen from './pages/welcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
