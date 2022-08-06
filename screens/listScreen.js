2ffd347a0a5b31e5761744893889180189bdb3e0
Margeaux DeRaedt <margderaedt@gmail.com>
2022-08-06 18:07:28 -0500

    add error state, use scroll view for list screen

--- screens/listScreen.js
+++ screens/listScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import { theme } from '../styles/stylesheet';
 import ThemedHeader from '../components/ThemedHeader/index';
 import ThemedLink from '../components/ThemedLink/index';
@@ -7,28 +7,36 @@ import ThemedText from '../components/ThemedText/index';
 import { NASA_API_URL, NASA_API_KEY } from '@env'
 
 const ListScreen = ({ navigation, route }) => {
+  const [nearEarthObjects, setNearEarthObjects] = useState(null);
+  const [error, setError] = useState(null);
   const { date } = route.params;
-  let [nearEarthObjects, setNearEarthObjects] = useState(null)
   const apiUrl = `${NASA_API_URL}?start_date=${date}&end_date=${date}&api_key=${NASA_API_KEY}`;
 
   useEffect(() => {
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => setNearEarthObjects(data['near_earth_objects'][date]))
+    .catch((error) => { setError(error)})
   },[])
 
   return (
-    <View style={theme.container}>
-      {nearEarthObjects &&
+    <ScrollView contentContainerStyle={theme.container}>
+      {(nearEarthObjects && !error) &&
         <>
           <ThemedHeader text={`We found ${nearEarthObjects.length} NEO${nearEarthObjects.length > 1 ? 's' : ''} for ${date}!`} />
           <ThemedText text={'Select an object below.'} />
-          { nearEarthObjects.map((neo, index) => (
+          {nearEarthObjects.map((neo, index) => (
             <ThemedLink key={`neo-ref-link-${index}`} text={`NEO Reference ID: ${neo['neo_reference_id']}`} onPressLink={() => navigation.push('DetailScreen', { screen: 'DetailScreen', date: date, nearEarthObject: neo })} />
           ))}
         </>
       }
-    </View>
+      {error &&
+        <>
+          <ThemedHeader text={'Something went wrong...'} />
+          <ThemedText text={'Please refresh the app or try again later.'} />
+        </>
+      }
+    </ScrollView>
   );
 }
 
