53fc69fd158626eb3173ff35fc9bc878cacfe750
Margeaux DeRaedt <margderaedt@gmail.com>
2022-08-06 18:43:08 -0500

    flesh out detail screen

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, ImageBackground, View, Text } from 'react-native';
 import { theme } from '../styles/stylesheet';
 import ThemedHeader from '../components/ThemedHeader/index';
 import ThemedButton from '../components/ThemedButton/index';
+import WelcomeScreenBackground from '../assets/welcome-screen-background.jpg';
 
 const DetailScreen = ({ navigation, route }) => {
   const { date, nearEarthObject } = route.params;
 
   return (
-    <View style={theme.container}>
-      <ThemedHeader text={nearEarthObject['neo_reference_id']} />
-      <ThemedButton
-        text={'Find NEOs!'}
-        // onPressButton={() => navigation.push('WatchScreen', { screen: 'WatchScreen' })}
-      />
-    </View>
+    <ScrollView contentContainerStyle={theme.container}>
+      <ImageBackground source={WelcomeScreenBackground} style={theme.homeScreenImage}>
+        <ThemedHeader text={`GET TO KNOW NEO ${nearEarthObject['name']}!`} />
+        <View>
+          <Text style={theme.keyText}>Name:</Text>
+          <Text style={theme.valueText}>{nearEarthObject['name']}</Text>
+        </View>
+        <View>
+          <Text style={theme.keyText}>Diameter (in feet):</Text>
+          <Text style={theme.valueText}>min({nearEarthObject['estimated_diameter']['feet']['estimated_diameter_min']})</Text>
+          <Text style={theme.valueText}>max({nearEarthObject['estimated_diameter']['feet']['estimated_diameter_max']})</Text>
+        </View>
+        <View>
+          <Text style={theme.keyText}>Miss Distance (in miles)</Text>
+          <Text style={theme.valueText}>{nearEarthObject['close_approach_data'][0]['miss_distance']['miles']}</Text>
+        </View>
+        <View>
+          <Text style={theme.keyText}>Speed (in MPH)</Text>
+          <Text style={theme.valueText}>{nearEarthObject['close_approach_data'][0]['relative_velocity']['miles_per_hour']}</Text>
+        </View>
+        <View>
+          <Text style={theme.keyText}>Potentially Hazardous?</Text>
+          <Text style={theme.valueText}>{nearEarthObject['is_potentially_hazardous_asteroid']}</Text>
+        </View>
+        <ThemedButton
+          text={'Back to List'}
+          onPressButton={() => navigation.goBack()}
+        />
+        <ThemedButton
+          text={'Select A New Date'}
+          onPressButton={() => navigation.push('FindScreen', { screen: 'FindScreen' })}
+        />
+      </ImageBackground>
+    </ScrollView>
   );
 }
 
